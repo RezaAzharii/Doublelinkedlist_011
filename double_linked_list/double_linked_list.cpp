1f185a5ba4f402ad0f2feed4b9c61d471e6576e7
RezaAzharii <rezaazhari867@gmail.com>
2023-05-25 14:07:22 +0700

    mendeklarasikan method searchData

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -144,3 +144,22 @@ void Doublelinkedlist::hapus() {
 	else
 		cout << "record with roll number" << rollNo << " deleted" << endl;
 }
+
+void Doublelinkedlist::searchData() {
+	if (listEmpty() == true) {
+		cout << "\nList is empty" << endl;
+
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (Doublelinkedlist::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
+}
