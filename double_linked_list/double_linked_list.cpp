e80400175ff36143761abe3cacab568c30fdf682
RezaAzharii <rezaazhari867@gmail.com>
2023-05-25 14:05:05 +0700

    mendeklarasi method ascending

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -100,3 +100,16 @@ bool Doublelinkedlist::deleteNode(int rollNo) {
 bool Doublelinkedlist::listEmpty() {
 	return (START == NULL);
 }
+
+void Doublelinkedlist::ascending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
